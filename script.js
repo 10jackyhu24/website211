9ba92279457b169750543a6c4d47da8c2d38b99a
10jackyhu24 <1024jacky@gmail.com>
2022-05-23 21:47:39 +0800

    add web page

---
+++ script.js
@@ -0,0 +1,26 @@
+function link_to(element){
+    if(element == 'previous_page'){
+        parent.location.href='../../../index.html';
+    }
+    if(element == 'producer'){
+        parent.location.href='../people/producer.html';
+    }
+    if(element == 'hujinwei'){
+        parent.location.href='../people/hujinwei.html';
+    }
+    if(element == 'panguanwei'){
+        parent.location.href='../people/panguanwei.html';
+    }
+    if(element == 'linyichen'){
+        parent.location.href='../people/linyichen.html';
+    }
+    if(element == 'caishaohua'){
+        parent.location.href='../people/caishaohua.html';
+    }
+    if(element == 'chenzichen'){
+        parent.location.href='../people/chenzichen.html';
+    }
+    if(element == 'ethan'){
+        parent.location.href='../people/ethan.html';
+    }
+}
